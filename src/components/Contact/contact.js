1ed3ae736c9d2070cc61e370581c83cdeaf4d07e
kimtimmy93 <kimtimmy93@gmail.com>
2020-01-23 01:58:34 -0800

    install font awesome

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 class Contact extends Component {
     render(){
@@ -6,6 +8,11 @@ class Contact extends Component {
             <div className="contact-section" id="contact">
                 <div className="contact__container">
                     <h2 className="contact__h2">Contact</h2>
+                </div>
+                <div className="contact__icons">
+                    <i class="fab fa-github"></i>
+                    <i class="fab fa-linkedin"></i>
+                    <i class="fab fa-envelope-o"></i>
                 </div>
             </div>
         )
