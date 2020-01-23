6b30dcf5d1e91f271ab4c4a81e1617ae076c0f45
kimtimmy93 <kimtimmy93@gmail.com>
2020-01-23 00:08:52 -0800

    add dev icons

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -6,6 +6,25 @@ class Skills extends Component {
             <div className="skills-section" id="skills">
                 <div className="skills_container">
                     <h2>Skills</h2>
+                    <div className="icons">
+                        <div className="first-half-icons">
+                            <i className="devicon-html5-plain-wordmark colored"></i>
+                            <i className="devicon-css3-plain-wordmark colored"></i>
+                            <i className="devicon-bootstrap-plain-wordmark colored"></i>
+                            <i className="devicon-javascript-plain colored"></i>
+                            <i className="devicon-jquery-plain-wordmark colored"></i>
+                            <i className="devicon-react-original-wordmark colored"></i>
+                            <i className="devicon-nodejs-plain-wordmark colored"></i>
+                        </div>
+                        <div className="second-half-icons">
+                            <i className="devicon-express-original-wordmark colored"></i>
+                            <i className="devicon-mongodb-plain-wordmark colored"></i>
+                            <i className="devicon-postgresql-plain-wordmark colored"></i>
+                            <i className="devicon-python-plain-wordmark colored"></i>
+                            <i className="devicon-git-plain-wordmark colored"></i>
+                            <i className="devicon-github-plain-wordmark colored"></i>
+                        </div>
+                    </div>
                 </div>
             </div>
             
