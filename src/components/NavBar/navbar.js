d239dc0d825273d659124b7b24c6f78430f06483
kimtimmy93 <kimtimmy93@gmail.com>
2020-01-22 17:16:34 -0800

    change a tags to Link

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch, Link } from 'react-router-dom'
 import './navbar.scss';
 
 class NavBar extends Component {
@@ -7,17 +8,17 @@ class NavBar extends Component {
             <div className="nav">
                 <div className="nav__header">
                     <div className="nav__left">
-                        <a href="/" className="nav__link">Timothy Kim</a>
+                        <Link to="/" className="nav__link">Timothy Kim</Link>
                     </div>
                     <div className="nav__center">
                     </div>
                     <div className="nav__right">
-                        <a href="/" className="nav__link">Home</a>
-                        <a href="/aboutme" className="nav__link">About Me</a>
-                        <a href="/skills" className="nav__link">Skills</a>
-                        <a href="/projects" className="nav__link">Projects</a>
-                        <a href="/resume" className="nav__link">Resume</a>
-                        <a href="/contact" className="nav__link">Contact</a>
+                        <Link to="/" className="nav__link">Home</Link>
+                        <Link to="/aboutme" className="nav__link">About Me</Link>
+                        <Link to="/skills" className="nav__link">Skills</Link>
+                        <Link to="/projects" className="nav__link">Projects</Link>
+                        <Link to="/resume" className="nav__link">Resume</Link>
+                        <Link to="/contact" className="nav__link">Contact</Link>
                         <div class="hamburger-menu">
                             <div class="bar"></div>
                             <div class="bar"></div>
@@ -25,14 +26,14 @@ class NavBar extends Component {
                         </div>
                     </div>
                 </div>
-                <div class="overlay">
+                {/* <div class="overlay">
                     <div class="overlay__content">
                         <a href="/aboutme" class="overlay__link">About Me</a>
                         <a href="/skills" class="overlay__link">Skills</a>
                         <a href="/projects" class="overlay__link">Projects</a>
                         <a href="/resume" class="overlay__link">Resume</a>
                     </div>
-                </div>
+                </div> */}
             </div>
         )
     }
