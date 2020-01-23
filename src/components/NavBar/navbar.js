e847abd5b7379f2b0a2634ebd5fa41ebb4801fdb
kimtimmy93 <kimtimmy93@gmail.com>
2020-01-22 16:36:43 -0800

    style nav

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,14 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './navbar.scss';
 
 class NavBar extends Component {
     render(){
         return(
-            <div>
-                <nav className="navbar">
-                    <a className="navbar__home" href="#">
-                        <h1>Home</h1>
-                    </a>
-                </nav>
+            <div className="nav">
+                <div className="nav__header">
+                    <div className="nav__left">
+                        <a href="/" className="nav__link">Logo</a>
+                    </div>
+                    <div className="nav__center">
+                    </div>
+                    <div className="nav__right">
+                        <a href="/" className="nav__link">Home</a>
+                        <a href="/aboutme" className="nav__link">About Me</a>
+                        <a href="/skills" className="nav__link">Skills</a>
+                        <a href="/projects" className="nav__link">Projects</a>
+                        <a href="/resume" className="nav__link">Resume</a>
+                        <a href="/contact" className="nav__link">Contact</a>
+                        <div class="hamburger-menu">
+                            <div class="bar"></div>
+                            <div class="bar"></div>
+                            <div class="bar"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="overlay">
+                    <div class="overlay__content">
+                        <a href="#" class="overlay__link">One</a>
+                        <a href="#" class="overlay__link">Two</a>
+                        <a href="#" class="overlay__link">Three</a>
+                        <a href="#" class="overlay__link">Four</a>
+                    </div>
+                </div>
             </div>
         )
     }
