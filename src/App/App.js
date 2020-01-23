4a3f7760edc859abed0d8194c69ad037d1a86796
kimtimmy93 <kimtimmy93@gmail.com>
2020-01-22 19:46:46 -0800

    install react scroll

--- src/App/App.js
+++ src/App/App.js
@@ -21,8 +21,6 @@ function App() {
         <Projects />
         <Contact />
         <Resume />
-        
-
     </div>
   );
 }
--- src/components/Landing/landing.js
+++ src/components/Landing/landing.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import '../../assets/code.jpg'
 
 class Landing extends Component {
     render(){
         return(
             <div style={{width: '100%', margin: 'auto'}}>
-                <h1>Landing</h1>
+                <img src="code.jpg" />
             </div>
         )
     }
