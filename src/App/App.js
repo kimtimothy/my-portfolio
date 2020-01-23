20fbc11a22a9c2e8abb1e80db6c58251d57e764d
kimtimmy93 <kimtimmy93@gmail.com>
2020-01-22 18:01:15 -0800

    have components at app level

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import Main from '../components/Main/main'
-import { Link } from 'react-router-dom'
 import NavBar from '../components/NavBar/navbar'
+import Main from '../components/Main/main'
+import AboutMe from '../components/AboutMe/aboutme'
+import Landing from '../components/Landing/landing'
+import Skills from '../components/Skills/skills'
+import Projects from '../components/Projects/projects'
+import Contact from '../components/Contact/contact'
+import Resume from '../components/Resume/resume'
+import Footer from '../components/Footer/footer'
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
     <div >
         <NavBar />
+        <Landing />
         <Main />
+        <AboutMe />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Resume />
         
+
     </div>
   );
 }
