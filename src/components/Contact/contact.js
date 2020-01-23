909f479cfdca3493fba81a5d276047a04ed76493
kimtimmy93 <kimtimmy93@gmail.com>
2020-01-23 03:15:01 -0800

    style fa icons

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -9,14 +9,16 @@ class Contact extends Component {
                     <h2 className="contact__h2">Contact</h2>
                 </div>
                 <div className="contact__icons">
-                    <a href="https://linkedin.com/in/timhkim" target="_blank" className="icon__github">
-                        <FaGithub  style={{color: "white", fontSize: "40pt"}}/>
+                    <a href="http://github.com/kimtimmy93" target="_blank" className="icon__github">
+                        <FaGithub  style={{color: "white", fontSize: "30pt"}}/>
                     </a>
-                    <a href="http://github.com/kimtimmy93" target="_blank" className="icon__linkedin">
-                        <FaLinkedinIn  style={{color: "white", fontSize: "40pt"}}/>
+                    <div className="icon__border-1" />
+                    <a href="https://linkedin.com/in/timhkim" target="_blank" className="icon__linkedin">
+                        <FaLinkedinIn  style={{color: "white", fontSize: "30pt"}}/>
                     </a>
+                    <div className="icon__border-2" />
                     <a href="mailto:timhkim0623@gmail.com" target="_blank" className="icon__envelope">
-                        <FaEnvelope  style={{color: "white", fontSize: "40pt"}}/>
+                        <FaEnvelope  style={{color: "white", fontSize: "30pt"}}/>
                     </a>
                 </div>
             </div>
